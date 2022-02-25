5d425557134b0e081782dd9bcc600865a2aae8b6
ksenakss <94202537+ksenakss@users.noreply.github.com>
2022-02-25 03:23:46 +0300

    Create CV

---
+++ app.js
@@ -0,0 +1,38 @@
+// let name = prompt("Your name?")
+// alert(`Your name is ${name}`)
+// alert("Я JavaScript!!!!!!!!!!!!!!!!")
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+
+// alert(+a + +b); // 12
+
+// let off_name = prompt(`What's the "официальное название" JavaScript?`)
+// if (off_name > 0) {
+//     alert("1")
+// } else if (off_name < 0){
+//     alert("-1")
+// } else {
+//     alert("0")
+// }
+
+// let message;
+// let login;
+// login = prompt("Your login", "")
+
+// message = (login == 'Сотрудник') ? message = 'Привет':
+//     (login == 'Директор') ? message = 'Здравствуйте':
+//     (login == '') ? 'Нет логина':
+//     message = '';
+// alert(message)
+
+// if (1 > 0 || 2 > 3) {
+//     alert("gay")
+// }
+
+// let age = prompt("Age", "");
+// if (!(age >= 14 && age <= 90)) {
+//     alert("YEEEAH")
+// }
+
+let admin = prompt("Who's here?", "");
+
